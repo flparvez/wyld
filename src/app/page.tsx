1e3586be778a9579b0bc58a416ca65efdac8d90b
flparvez <flparvez23@gmail.com>
2024-03-18 02:29:05 +0600

    project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { HeroParallaxDemo } from "@/components/Hero";
+import { NavbarDemo } from "@/components/Navbar";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <NavbarDemo />
+      <HeroParallaxDemo />
+    </div>
+  );
+};
+
+export default Home;
