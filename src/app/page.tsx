ca0abdadd56ab5486c0903f01418039677b7debc
flparvez <flparvez23@gmail.com>
2024-03-18 22:13:04 +0600

    Navbar Add sticky fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { HeroParallaxDemo } from "@/components/Hero";
-import { NavbarDemo } from "@/components/Navbar";
+import Navbar from "@/components/Navbar";
+
 import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <NavbarDemo />
+      <Navbar />
       <HeroParallaxDemo />
     </div>
   );
