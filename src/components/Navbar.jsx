65d59beabde9c4aaeddba978c86d7db23c5d2af8
flparvez <flparvez23@gmail.com>
2024-03-18 22:11:48 +0600

    Add Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,152 @@
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import Logo from "../../public/logo.jpeg";
+import Link from "next/link";
+import {
+  AiOutlineMenu,
+  AiOutlineClose,
+  AiOutlineInstagram,
+  AiOutlineFacebook,
+  AiOutlineTwitter,
+} from "react-icons/ai";
+
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const handleNav = () => {
+    setMenuOpen(!menuOpen);
+  };
+  return (
+    <nav className="fixed z-10 w-full h-24 shadow-xl  bg-[#020d17] ">
+      <div className="flex justify-between items-center h-full w-full px-4 2xl:px-16">
+        <div className="w-14">
+          <Image
+            src={Logo}
+            alt=""
+            width={205}
+            height={25}
+            className="cursor-pointer"
+            priority
+          />
+        </div>
+
+        <div className="hidden sm:flex">
+          <ul className="hidden sm:flex">
+            <Link href="/about">
+              <li className="ml-10 uppercase hover:border-b text-xl">Brands</li>
+            </Link>
+
+            <Link href="/about">
+              <li className="ml-10 uppercase hover:border-b text-xl">Blogs</li>
+            </Link>
+
+            <Link href="/about">
+              <li className="ml-10 uppercase hover:border-b text-xl">
+                Investors
+              </li>
+            </Link>
+            <Link href="/about">
+              <li className="ml-10 uppercase hover:border-b text-xl">
+                Academy
+              </li>
+            </Link>
+            <Link href="/about">
+              <li className="ml-10 uppercase hover:border-b text-xl">Faqs</li>
+            </Link>
+          </ul>
+        </div>
+        <div className="sm:hidden cursor-pointer">
+          <ul className="flex ">
+            <Link href="/login">
+              <li className="ml-10 uppercase hover:border-b text-xl">Login</li>
+            </Link>
+
+            <Link href="/register">
+              <li className="ml-10 uppercase hover:border-b text-xl">Signup</li>
+            </Link>
+          </ul>
+        </div>
+        <div onClick={handleNav} className="sm:hidden cursor-pointer pl-24">
+          <AiOutlineMenu size={25} />
+        </div>
+      </div>
+
+      <div
+        className={
+          menuOpen
+            ? "fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-[#020d17] p-10 case-in duration-500"
+            : "fixed left-[-100%] top-0 p-10 case-in duration-500"
+        }
+      >
+        <div className="flex w-full items-center justify-end">
+          <div onClick={handleNav} className="cursor-pointer">
+            <AiOutlineClose size={25} />
+          </div>
+        </div>
+
+        <div className="flex-col py-4 bg-[#020d17]">
+          <ul>
+            <Link href="/">
+              <li
+                onClick={() => setMenuOpen(false)}
+                className="py-4 cursor-pointer"
+              >
+                Brands
+              </li>
+            </Link>
+
+            <Link href="/">
+              <li
+                onClick={() => setMenuOpen(false)}
+                className="py-4 cursor-pointer"
+              >
+                Investors
+              </li>
+            </Link>
+
+            <Link href="/">
+              <li
+                onClick={() => setMenuOpen(false)}
+                className="py-4 cursor-pointer"
+              >
+                Academy
+              </li>
+            </Link>
+            <Link href="/">
+              <li
+                onClick={() => setMenuOpen(false)}
+                className="py-4 cursor-pointer"
+              >
+                Faqs
+              </li>
+            </Link>
+
+            <Link href="/">
+              <li
+                onClick={() => setMenuOpen(false)}
+                className="py-4 cursor-pointer"
+              >
+                Contact
+              </li>
+            </Link>
+          </ul>
+        </div>
+        <div className="flex flex-row justify-around pt-10 items-center">
+          <AiOutlineInstagram size={30} className="cursor-pointer" />
+          <AiOutlineFacebook size={30} className="cursor-pointer" />
+          <AiOutlineTwitter size={30} className="cursor-pointer" />
+        </div>
+        <Image
+          src={Logo}
+          alt=""
+          width={105}
+          height={25}
+          className="cursor-pointer"
+          priority
+        />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
