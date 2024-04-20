58b088b27e23ec57531f3b7acdc663b252ba2629
flparvez <flparvez23@gmail.com>
2024-04-20 09:42:14 +0600

    bug fix

--- src/app/auth/profile/[userid]/page.tsx
+++ src/app/auth/profile/[userid]/page.tsx
@@ -4,11 +4,19 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function UserProfile({ params }: any) {
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
+  const [username, setUsername] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [number, setNumber] = useState(null);
+
+  // const { username, email, number }: any = data;
   const getUserDetails = async () => {
     const response = await axios.get("/api/users/me");
-    console.log(response.data);
-    setData(response.data.data);
+    // console.log(response.data);
+    // setData(response.data.data);
+    setUsername(response.data.data.username);
+    setEmail(response.data.data.email);
+    setNumber(response.data.data.number);
   };
   useEffect(() => {
     getUserDetails();
@@ -20,9 +28,14 @@ export default function UserProfile({ params }: any) {
       <hr />
       <p className="text-4xl mt-10">
         <span className=" p-2 ml-2 rounded bg-orange-500 text-black">
-          Name:{data?.username}
+          UserId : {params.userid}
+        </span>
+        <hr />
+        <span className=" p-2 ml-2 rounded bg-orange-500 text-black">
+          Username : {username}
         </span>
       </p>
     </div>
   );
+  s;
 }
